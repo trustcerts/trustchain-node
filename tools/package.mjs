6f1c1965999e34aa4656f9356fc37d7e91cbdc92
Mirko Mollik <mollik@trustcerts.de>
2021-11-22 16:40:28 +0100

    ci

--- tools/package.mjs
+++ tools/package.mjs
@@ -6,11 +6,11 @@ const TOKEN = process.env.NPM_TOKEN;
 
 (async () => {
   await run(
-    `docker build -f ./build/baseimage.Dockerfile -t baseimage --build-arg NPM_TOKEN=${TOKEN} .`,
+    `docker build --pull -f ./build/baseimage.Dockerfile -t baseimage --build-arg NPM_TOKEN=${TOKEN} .`,
   );
   for (let project of projects) {
     await run(
-      `docker build --build-arg app=${project} -f ./build/Dockerfile -t ${CONTAINER}/${project}:${CI_TAG} .`,
+      `docker build --pull --build-arg app=${project} -f ./build/Dockerfile -t ${CONTAINER}/${project}:${CI_TAG} .`,
     );
   }
 })();
