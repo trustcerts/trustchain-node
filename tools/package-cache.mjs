c2dc90bb241d24007e9064b65e589214c1117c0b
Mustafa Alsalfiti <alsalfiti@trustcerts.de>
2021-12-08 14:35:37 +0100

    fix cache

--- tools/package-cache.mjs
+++ tools/package-cache.mjs
@@ -4,25 +4,29 @@ const CONTAINER = process.env.CI_REGISTRY_IMAGE;
 const CI_TAG = process.env.CI_COMMIT_BRANCH;
 
 (async () => {
+  // create a dir to save the tar files inside and then to cache it
+  await run(
+    `mkdir dockerbuild `,
+  );
   // build base image
   await run(
     `docker build --cache-from -f ./build/baseimage.Dockerfile -t trustcerts/trustchain-baseimage:${CI_TAG} .`,
   );
   // save image as a tar file
   await run(
-    `docker save -o -f /shared/dockercache/trustchain-baseimage.tar -t trustcerts/trustchain-baseimage:${CI_TAG} .`,
+    `docker save -o ./dockerbuild/trustchain-baseimage.tar -t trustcerts/trustchain-baseimage:${CI_TAG} .`,
   );
 
   // build dev image
   await run(
     `docker build --cache-from -f ./build/base-dev.Dockerfile -t trustcerts/trustchain-dev:${CI_TAG} .`,
   );
-  await run(`docker save -o /shared/dockercache/trustchain-dev.tar trustcerts/trustchain-dev:${CI_TAG}`);
+  await run(`docker save -o ./dockerbuild/trustchain-dev.tar trustcerts/trustchain-dev:${CI_TAG}`);
 
   for (let project of projects) {
     await run(
-      `docker build --build-arg app=${project} --build-arg base=${CI_TAG} -f ./build/Dockerfile -t ${CONTAINER}-${project}:${CI_TAG} .`,
+      `docker build --cache-from --build-arg app=${project} --build-arg base=${CI_TAG} -f ./build/Dockerfile -t ${CONTAINER}-${project}:${CI_TAG} .`,
     );
-    await run(`docker save -o /shared/dockercache/${CONTAINER}-${project}.tar ${CONTAINER}-${project}:${CI_TAG}`);
+    await run(`docker save -o ./dockerbuild/${CONTAINER}-${project}.tar ${CONTAINER}-${project}:${CI_TAG}`);
   }
 })();
