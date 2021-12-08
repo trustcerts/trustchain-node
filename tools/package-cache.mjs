eaef5eb0e406ff14a5925f0c347f28b8cf2aab56
Mustafa Alsalfiti <alsalfiti@trustcerts.de>
2021-12-08 17:00:24 +0100

    update cache

--- tools/package-cache.mjs
+++ tools/package-cache.mjs
@@ -10,23 +10,20 @@ const CI_TAG = process.env.CI_COMMIT_BRANCH;
   );
   // build base image
   await run(
-    `docker build --cache-from -f ./build/baseimage.Dockerfile -t trustcerts/trustchain-baseimage:${CI_TAG} .`,
+    `docker build --cache-from trustcerts/trustchain-baseimage:${CI_TAG} -f ./build/baseimage.Dockerfile -t trustcerts/trustchain-baseimage:${CI_TAG} .`,
   );
-  // save image as a tar file
-  await run(
-    `docker save -o ./dockerbuild/trustchain-baseimage.tar -t trustcerts/trustchain-baseimage:${CI_TAG} .`,
-  );
-
   // build dev image
   await run(
-    `docker build --cache-from -f ./build/base-dev.Dockerfile -t trustcerts/trustchain-dev:${CI_TAG} .`,
+    `docker build --cache-from trustcerts/trustchain-dev:${CI_TAG} -f ./build/base-dev.Dockerfile -t trustcerts/trustchain-dev:${CI_TAG} .`,
   );
-  await run(`docker save -o ./dockerbuild/trustchain-dev.tar trustcerts/trustchain-dev:${CI_TAG}`);
+
+  // save image as a tar file
+  await run(`docker save trustcerts/trustchain-dev:${CI_TAG} > ./dockerbuild/trustchain-dev.tar trustcerts/trustchain-dev:${CI_TAG}`);
 
   for (let project of projects) {
     await run(
-      `docker build --cache-from --build-arg app=${project} --build-arg base=${CI_TAG} -f ./build/Dockerfile -t ${CONTAINER}-${project}:${CI_TAG} .`,
+      `docker build --cache-from ${CONTAINER}-${project}:${CI_TAG} --build-arg app=${project} --build-arg base=${CI_TAG} -f ./build/Dockerfile -t ${CONTAINER}-${project}:${CI_TAG} .`,
     );
-    await run(`docker save -o ./dockerbuild/${CONTAINER}-${project}.tar ${CONTAINER}-${project}:${CI_TAG}`);
+    await run(`docker save  ${CONTAINER}-${project}:${CI_TAG} > ./dockerbuild/${CONTAINER}-${project}.tar`);
   }
 })();
