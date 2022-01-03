db27e0361260ea0aabc24181de83debf8219e606
Mustafa Alsalfiti <alsalfiti@trustcerts.de>
2022-01-03 12:26:48 +0100

    update

--- tools/save-docker-images.mjs
+++ tools/save-docker-images.mjs
@@ -8,6 +8,15 @@ const CI_TAG = process.env.CI_COMMIT_BRANCH;
     if(!fs.existsSync(`./docker-dist`)){
         mkdirSync('docker-dist')
     }
+
+    await run(
+        `docker save trustcerts/trustchain-baseimage:${CI_TAG} > ./docker-dist/trustchain-baseimage.tar`
+    )
+
+    await run(
+        `docker save trustcerts/trustchain-dev:${CI_TAG} > ./docker-dist/trustchain-dev.tar`
+    )
+
     for (let project of projects) {
     await run(
         `docker save ${CONTAINER}-${project}:${CI_TAG} > ./docker-dist/trustchain-${project}.tar`
