327059c193ff90c7b9678d35176847a5cce275db
Mirko Mollik <mollik@trustcerts.de>
2022-01-10 14:04:42 +0100

    fix: optimize ci for release
    Signed-off-by: Mirko Mollik <mollik@trustcerts.de>

--- tools/retag.mjs
+++ tools/retag.mjs
@@ -1,17 +1,12 @@
 import { projects, run } from './functions.mjs';
 
-const OLD_CONTAINER = process.env.CI_REGISTRY_IMAGE;
-const NEW_CONTAINER = OLD_CONTAINER.split('/').slice(1).join('/');
-const CI_TAG = process.env.CI_COMMIT_BRANCH;
-const TAG_RELEASE = process.env.TAG_RELEASE | 'beta';
+const CI_TAG = process.env.CI_TAG;
+const TAG_RELEASE = process.env.TAG_RELEASE;
 (async () => {
-  // build base image
   for (let project of projects) {
-    await run(`docker pull ${OLD_CONTAINER}-${project}:${CI_TAG}`);
     await run(
-      `docker tag ${OLD_CONTAINER}-${project}:${CI_TAG} ${NEW_CONTAINER}-${project}:${TAG_RELEASE}`,
+      `docker tag trustcerts/trustchain-${project}:${CI_TAG} trustcerts/trustchain-${project}:${TAG_RELEASE}`,
     );
-    await run(`docker push ${NEW_CONTAINER}-${project}:${TAG_RELEASE}`);
-    // TODO remove old image from the github registry.
+    await run(`docker push trustcerts/trustchain-${project}:${TAG_RELEASE}`);
   }
 })();
