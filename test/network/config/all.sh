608a0e599052980d4589054617f6a69dbda708ab
Mirko Mollik <mollik@trustcerts.de>
2022-01-31 15:41:57 +0100

    fix

---
+++ test/network/config/all.sh
@@ -0,0 +1,4 @@
+for id in validator1 validator2 validator3 validator4 gateway1 gateway2 observer1 observer2 
+do
+    ./$id.sh $@
+done
---
+++ test/network/config/validator3.sh
@@ -0,0 +1,12 @@
+NODE_PATH='./trustchain-config/nodes'
+docker-compose \
+    -f "$NODE_PATH/docker-compose.yml" \    
+    -f "$NODE_PATH/http/docker-compose.port.yml" \    
+    -f "$NODE_PATH/loki/docker-compose.yml" \
+    -f "$NODE_PATH/database/docker-compose.yml" \
+    -f "$NODE_PATH/database/docker-compose.port.yml" \
+    -f "$NODE_PATH/validator/docker-compose.yml" \
+    -f "$NODE_PATH/validator/docker-compose.port.yml" \    
+    -f "$NODE_PATH/docker-compose.docker.yml" \
+    --env-file './validator3.env' \
+    $@
