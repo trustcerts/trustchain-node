fa7d494d46749c0134f9df0fb8e4738e7f7f45d7
Mirko Mollik <mollik@trustcerts.de>
2022-02-21 08:37:08 +0100

    fix: network test
    Signed-off-by: Mirko Mollik <mollik@trustcerts.de>

--- test/network/config/initNodes.sh
+++ test/network/config/initNodes.sh
@@ -17,11 +17,11 @@ echo "invite observer" &
 curl -X POST "http://localhost:4501/did/invite" -H "accept: */*" -H "Authorization: Bearer test" -H "Content-Type: application/json" -d "{\"id\":\"did:trust:tc:dev:id:FDPrCrhPCnVPgSyWEfQyJj\",\"name\":\"Observer1\",\"secret\":\"foobar\",\"role\":\"observer\"}"
 curl -X POST "http://localhost:4501/did/invite" -H "accept: */*" -H "Authorization: Bearer test" -H "Content-Type: application/json" -d "{\"id\":\"did:trust:tc:dev:id:6xNu7M86KFu4B4hYNNg2GD\",\"name\":\"Observer2\",\"secret\":\"foobar\",\"role\":\"observer\"}"
 echo "add gateway" &
-curl -X POST "http://localhost:4541/init" -H "accept: */*" -H "Authorization: Bearer test" -H "Content-Type: application/json" -d "{\"id\":\"did:trust:tc:dev:id:4GqRUNp2wRgnHAPUZb8X9X\",\"secret\":\"foobar\",\"url\":\"validator1_http_1:3000\"}"
-curl -X POST "http://localhost:4551/init" -H "accept: */*" -H "Authorization: Bearer test" -H "Content-Type: application/json" -d "{\"id\":\"did:trust:tc:dev:id:BfjXrhHYXJd7Rg9HaD3ocX\",\"secret\":\"foobar\",\"url\":\"validator1_http_1:3000\"}"
+curl -X POST "http://localhost:4541/init" -H "accept: */*" -H "Authorization: Bearer test" -H "Content-Type: application/json" -d "{\"id\":\"did:trust:tc:dev:id:4GqRUNp2wRgnHAPUZb8X9X\",\"secret\":\"foobar\",\"url\":\"testing-validator1_http_1:3000\"}"
+curl -X POST "http://localhost:4551/init" -H "accept: */*" -H "Authorization: Bearer test" -H "Content-Type: application/json" -d "{\"id\":\"did:trust:tc:dev:id:BfjXrhHYXJd7Rg9HaD3ocX\",\"secret\":\"foobar\",\"url\":\"testing-validator1_http_1:3000\"}"
 echo "add observer" &
-curl -X POST "http://localhost:4561/init" -H "accept: */*" -H "Authorization: Bearer test" -H "Content-Type: application/json" -d "{\"id\":\"did:trust:tc:dev:id:FDPrCrhPCnVPgSyWEfQyJj\",\"secret\":\"foobar\",\"url\":\"validator1_http_1:3000\"}"
-curl -X POST "http://localhost:4571/init" -H "accept: */*" -H "Authorization: Bearer test" -H "Content-Type: application/json" -d "{\"id\":\"did:trust:tc:dev:id:6xNu7M86KFu4B4hYNNg2GD\",\"secret\":\"foobar\",\"url\":\"validator1_http_1:3000\"}"
+curl -X POST "http://localhost:4561/init" -H "accept: */*" -H "Authorization: Bearer test" -H "Content-Type: application/json" -d "{\"id\":\"did:trust:tc:dev:id:FDPrCrhPCnVPgSyWEfQyJj\",\"secret\":\"foobar\",\"url\":\"testing-validator1_http_1:3000\"}"
+curl -X POST "http://localhost:4571/init" -H "accept: */*" -H "Authorization: Bearer test" -H "Content-Type: application/json" -d "{\"id\":\"did:trust:tc:dev:id:6xNu7M86KFu4B4hYNNg2GD\",\"secret\":\"foobar\",\"url\":\"testing-validator1_http_1:3000\"}"
 echo "add client"
 curl -X POST "http://localhost:4541/did/invite" -H "accept: */*" -H "Authorization: Bearer test" -H "Content-Type: application/json" -d "{\"id\":\"did:trust:tc:dev:id:XLzBJ69tqEgq7oqqCLiEnT\",\"name\":\"client\",\"secret\":\"client\",\"role\":\"client\"}" &
 curl -X POST "http://localhost:4541/did/invite" -H "accept: */*" -H "Authorization: Bearer test" -H "Content-Type: application/json" -d "{\"id\":\"did:trust:tc:dev:id:XLzBJ69tqEgHEALTHCHECK\",\"name\":\"Healthcheck\",\"secret\":\"Healthcheck\",\"role\":\"client\"}" &
