673aa0fb7fc25a2ad1cbe7a2d6e2526df089e0eb
Mirko Mollik <mollik@trustcerts.de>
2022-02-28 12:29:34 +0100

    fix: use config from submodule
    Signed-off-by: Mirko Mollik <mollik@trustcerts.de>

--- test/network/config/all.sh
+++
@@ -1,7 +0,0 @@
-docker network inspect test-network >/dev/null 2>&1 || \
-    docker network create test-network
-
-for id in validator1 validator2 validator3 validator4 gateway1 gateway2 observer1 observer2 
-do
-    ./$id.sh $@
-done
