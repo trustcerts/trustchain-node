03ca88a6bf13619bb39b8d944b9ed1eb1264314b
Mustafa Alsalfiti <mmsalfiti@gmail.com>
2022-03-29 13:57:43 +0000

    fix:update tests config

--- apps/http-validator/test/jest-e2e.ts
+++ apps/http-validator/test/jest-e2e.ts
@@ -8,6 +8,12 @@ const config: Config.InitialOptions = {
   ...jestConfig,
   rootDir: '../../../',
   testRegex: '.e2e-spec.ts$',
+  collectCoverageFrom: [
+    '<rootDir>/apps/http-validator/src/**',
+    '!<rootDir>/apps/http-validator/src/*.*.spec.ts',
+    '!<rootDir>/apps/http-validator/src/**/*.*.spec.ts',
+    '!<rootDir>/apps/http-validator/src/main.ts',
+  ],
   roots: ['<rootDir>/apps/http-validator/', '<rootDir>/libs/'],
 };
 
